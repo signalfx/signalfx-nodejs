823318e2f3ca578b553b8497703f824e54d481b3
Jakub Malinowski <jmalinowski+alt@splunk.com>
2022-09-08 21:51:08 +0200

    fix: mark ws as external

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = (_, argv) => ({
   mode: "production",
   devtool: "inline-source-map",
   entry: "./lib/signalfx_browser.js",
+  externals: [
+    "ws"
+  ],
   output: {
     filename: "signalfx.js",
     path: path.resolve(__dirname, "build"),
