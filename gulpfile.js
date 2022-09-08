655b5d14926163f455ce806dd2e0a8909cb1979b
Jakub Malinowski <jmalinowski+alt@splunk.com>
2022-09-08 21:13:58 +0200

    chore: move mocha out of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,13 @@
 'use strict';
 var gulp = require('gulp');
 var excludeGitignore = require('gulp-exclude-gitignore');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
-var plumber = require('gulp-plumber');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
 exports.nsp = function (cb) {
   nsp('package.json', cb);
 };
-
-function preTest() {
-  return gulp.src('lib/**/*.js')
-    .pipe(istanbul({includeUntested: true}))
-    .pipe(istanbul.hookRequire());
-}
-
-function test(cb) {
-  var mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'})) // nyan
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
-}
 
 exports.browserify = function () {
   //todo : minify
@@ -45,8 +21,4 @@ exports.browserify = function () {
     .pipe(gulp.dest('./build/'));
 };
 
-exports.prepublish = function prepublish(cb) {
-  cb();
-};
-exports.test = gulp.series(preTest, test);
-exports.default = gulp.series(test);
+exports.default = gulp.series(browserify);
