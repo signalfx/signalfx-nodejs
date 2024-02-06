d263cd7687f435caa606fee7ed4d5f55e260b1e5
Simon Fishel <64429613+sfishel-splunk@users.noreply.github.com>
2024-02-06 10:44:17 -0800

    feat(types): adds additional type definitions for Signalflow client, and ingest client (#113)
    1. includes additional code from Splunk's internal types for the Signalflow client
    1. includes types for ingest client from the `@types/signalfx` npm module

---
+++ lib/ingest.d.ts
@@ -0,0 +1,35 @@
+// Definitions by: Vladimir Grenaderov <https://github.com/VladimirGrenaderov>
+//                 Max Boguslavskiy <https://github.com/maxbogus>
+
+export interface IngestOptions {
+    enableAmazonUniqueId?: boolean | undefined;
+    dimensions?: object | undefined;
+    ingestEndpoint?: string | undefined;
+    timeout?: number | undefined;
+    batchSize?: number | undefined;
+    userAgents?: string[] | undefined;
+    proxy?: string | undefined;
+}
+
+export interface SignalMetric {
+    metric: string;
+    value: number;
+    timestamp?: number | undefined;
+    dimensions?: object | undefined;
+}
+
+export interface SignalReport {
+    cumulative_counters?: SignalMetric[] | undefined;
+    gauges?: SignalMetric[] | undefined;
+    counters?: SignalMetric[] | undefined;
+}
+
+export class Ingest {
+    constructor(token: string, options?: IngestOptions);
+    send(report: SignalReport): void;
+}
+
+export class IngestJson {
+    constructor(token: string, options?: IngestOptions);
+    send(report: SignalReport): void;
+}
